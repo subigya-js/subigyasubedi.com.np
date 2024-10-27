d2aac8c3e673607622de49eaec45643034ea2b08
SUBIGYA SUBEDI <developer.subigya@gmail.com>
2024-10-27 12:25:47 +0530

    Code setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-center px-4">
+        Website is updating with new technologies, till then visit{" "}
+        <span className="text-blue-500 underline">
+          <Link href={"https://subigyasubedi.com.np/"} target="__blank">Old Website</Link>
+        </span>
+      </h1>
+    </div>
+  );
+}
