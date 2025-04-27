721cdf2573cd955bc878bd93b055492117f512df
subigya-js <developer.subigya@gmail.com>
2025-04-28 00:18:31 +0530

    refactor: refactor project constants to update descriptions and reorder entries

--- app/constants.js
+++ app/constants.js
@@ -1,47 +1,31 @@
 export const projects = [
   {
+    id: "ae",
+    name: "Attend Ease",
+    desc: "A a full stack real-time face recognition attendance system that uses browser-based face detection with Hugging Face Transformers.js, enabling secure, client-side authentication without server dependencies.",
+    repo: "https://github.com/subigya-js/attend-ease",
+    deploy: "https://attend-ease-subigya.vercel.app/",
+  },
+  {
+    id: "at",
+    name: "Audio Transcriber",
+    desc: "A browser-based web application utilizing Hugging Face Transformers.js for client-side speech-to-text transcription with Whisper and real-time summarization using DistilBART models.",
+    repo: "https://github.com/subigya-js/audio-transcriber",
+    deploy: "https://audio-transcriber-subigya.vercel.app/",
+  },
+  {
     id: "et",
     name: "Expense Tracker",
-    desc: "A full-stack Expense Tracker built with Next.js for the frontend and Express.js for the backend.",
+    desc: "A full-stack application that enables users to categorize income and expenses, create custom transaction reports, visualize yearly expenses with bar graphs, and monitor financial trends for smarter budgeting.",
     repo: "https://github.com/subigya-js/expense-tracker",
     deploy: "https://expense-tracker-subigya.vercel.app/",
-  },
-  {
-    id: "bs",
-    name: "BlogSphere",
-    desc: "A Full Stack Blog Application built with NextJS, Express JS and MongoDB.",
-    repo: "https://github.com/subigya-js/BlogSphere",
-    deploy: "https://github.com/subigya-js/BlogSphere",
-  },
-
-  {
-    id: "cm",
-    name: "Contact Manager Rest API",
-    desc: "Contact Manager Application Backend built with Express JS.",
-    repo: "https://github.com/subigya-js/contact-manager",
-    deploy: "https://github.com/subigya-js/contact-manager",
   },
 
   {
     id: "ct",
     name: "Crypto Tracker",
-    desc: "A Web Application to track all the Crypto Currencies.",
+    desc: "A Web Application that allows users to search and view detailed market data of cryptocurrencies, including daily statistics and interactive line charts.",
     repo: "https://github.com/subigya-js/Cypto-Tracker",
     deploy: "https://crypto-tracker-subigya.netlify.app/",
-  },
-  {
-    id: "gpf",
-    name: "Github Profile Finder",
-    desc: "A Web Application to search for GitHub profiles and view their details.  ",
-    repo: "https://github.com/subigya-js/GitHub-Profile-Finder",
-    deploy: "https://github-profilee-finderr.netlify.app/",
-  },
-
-  {
-    id: "wa",
-    name: "Weather Application",
-    desc: "A Web Application to get the Weather update of a particular city.",
-    repo: "https://github.com/subigya-js/React-Weather-App/tree/main/weather-app",
-    deploy: "https://weather-app-subedi.netlify.app/",
   },
 ];
