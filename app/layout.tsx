5be87b4fa47080ccf977264ed3b57066b3174a6a
subigya-js <developer.subigya@gmail.com>
2025-10-01 22:19:16 +0545

    fix: reorder import statements and adjust layout for main content area

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import Navbar from "./components/Navbar";
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,7 +35,7 @@ export default function RootLayout({
         <div className="flex justify-center">
           <Navbar />
         </div>
-        <main className="flex-grow flex items-center justify-center overflow-y-auto" style={{ height: 'calc(100vh - 10rem)' }}>
+        <main className="flex-grow flex justify-center overflow-y-auto mt-36">
           <div className="w-full max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
             {children}
           </div>
